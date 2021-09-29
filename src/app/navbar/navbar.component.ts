e4b728f93a038be9e5f8aad43c0ed113a8ceb9d2
Oleksandr <s471652@gmail.com>
2021-09-29 14:07:29 +0300

    Displaying the Current User

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase";
 
 @Component({
   selector: "navbar",
@@ -7,8 +8,10 @@ import { AngularFireAuth } from "angularfire2/auth";
   styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent {
+  user: firebase.User;
+
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe((x) => console.log(x));
+    afAuth.authState.subscribe((user) => (this.user = user));
   }
 
   logout() {
