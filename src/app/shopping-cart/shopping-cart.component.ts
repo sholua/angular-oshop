76f86a7db323dca19acc7b7c68cf3e7355525a78
Oleksandr <s471652@gmail.com>
2021-11-14 14:25:17 +0200

    Building a Simple Shopping Cart Page

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ShoppingCartService } from "../shopping-cart.service";
+import { Observable } from "rxjs";
+import { ShoppingCart } from "../models/shopping-cart";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  selector: "app-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.css"],
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
   }
-
 }
