5df8f0dea9aa107ee7eb7b054d439cfe7bb4534b
Oleksandr <s471652@gmail.com>
2021-11-17 19:38:27 +0200

    Refactoring

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,47 +1,19 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ShoppingCartService } from "../shopping-cart.service";
 import { ShoppingCart } from "../models/shopping-cart";
-import { Subscription } from "rxjs";
-import { OrderService } from "../order.service";
-import { AuthService } from "../auth.service";
-import { Order } from "../models/order";
-import { Router } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "app-check-out",
   templateUrl: "./check-out.component.html",
   styleUrls: ["./check-out.component.css"],
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
-  shipping: {} = {};
-  cart: ShoppingCart;
-  userId: string;
-  cartSubscription: Subscription;
-  userSubscription: Subscription;
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private shopppingCartService: ShoppingCartService,
-    private orderService: OrderService
-  ) {}
+  constructor(private shopppingCartService: ShoppingCartService) {}
 
   async ngOnInit() {
-    const cart$ = await this.shopppingCartService.getCart();
-    this.cartSubscription = cart$.subscribe((cart) => (this.cart = cart));
-    this.userSubscription = this.authService.user$.subscribe(
-      (user) => (this.userId = user.uid)
-    );
-  }
-
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
-  }
-
-  async placeOrder() {
-    const order = new Order(this.userId, this.shipping, this.cart);
-    const result = await this.orderService.placeOrder(order);
-    this.router.navigate(["/order-success", result.key]);
+    this.cart$ = await this.shopppingCartService.getCart();
   }
 }
---
+++ src/app/shipping-form/shipping-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
+import { Order } from "../models/order";
+import { OrderService } from "../order.service";
+import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ShoppingCart } from "../models/shopping-cart";
+
+@Component({
+  selector: "shipping-form",
+  templateUrl: "./shipping-form.component.html",
+  styleUrls: ["./shipping-form.component.css"],
+})
+export class ShippingFormComponent implements OnInit, OnDestroy {
+  @Input("cart") cart: ShoppingCart;
+
+  shipping = {};
+  userId: string;
+  subscription: Subscription;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private orderService: OrderService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.user$.subscribe(
+      (user) => (this.userId = user.uid)
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  async placeOrder() {
+    const order = new Order(this.userId, this.shipping, this.cart);
+    const result = await this.orderService.placeOrder(order);
+    this.router.navigate(["/order-success", result.key]);
+  }
+}
