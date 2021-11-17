d6ddf179576d5fb971b46739c64d5978d5b9d357
Oleksandr <s471652@gmail.com>
2021-11-17 11:17:43 +0200

    Refactoring Extracting ProductQuantityComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { FormsModule } from "@angular/forms";
 import { ProductFilterComponent } from "./products/product-filter/product-filter.component";
 import { ProductCardComponent } from "./product-card/product-card.component";
 import { ShoppingCartService } from "./shopping-cart.service";
+import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
 
 @NgModule({
   declarations: [
@@ -51,6 +52,7 @@ import { ShoppingCartService } from "./shopping-cart.service";
     ProductFormComponent,
     ProductFilterComponent,
     ProductCardComponent,
+    ProductQuantityComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,4 +1,5 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
+import { Product } from "./product";
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
@@ -7,6 +8,11 @@ export class ShoppingCart {
       const item = itemsMap[productId];
       this.items.push(new ShoppingCartItem(item.product, item.quantity));
     }
+  }
+
+  getQuantiry(product: Product) {
+    const item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;
   }
 
   get totalPrice() {
