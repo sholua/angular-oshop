37b1454eacbebd93e2abe5c81a843da09a4e43ea
Oleksandr <s471652@gmail.com>
2021-11-18 11:09:25 +0200

    Displaying the Orders

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { OrderService } from "../order.service";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-my-orders',
-  templateUrl: './my-orders.component.html',
-  styleUrls: ['./my-orders.component.css']
+  selector: "app-my-orders",
+  templateUrl: "./my-orders.component.html",
+  styleUrls: ["./my-orders.component.css"],
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+  orders$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private orderService: OrderService,
+    private authService: AuthService
+  ) {
+    this.orders$ = authService.user$.switchMap((u) =>
+      orderService.getOrdersByUser(u.uid)
+    );
   }
-
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -9,6 +9,19 @@ export class OrderService {
     private shoppingCartService: ShoppingCartService
   ) {}
 
+  getOrders() {
+    return this.db.list("/orders");
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list("/orders", {
+      query: {
+        orderByChild: "userId",
+        equalTo: userId,
+      },
+    });
+  }
+
   async placeOrder(order) {
     const result = await this.db.list("/orders").push(order);
     this.shoppingCartService.clearCart();
